b31e7d70f3eb1ab3dc14c6e8368442c6d3edd36f
Scott Hetrick <Scott.Hetrick@la-archdiocese.org>
2018-07-16 17:03:59 -0700

    Moved angular source code up one directory; added FontAwesome CSS; installed Bootstrap scss; connected node server to MongoDB database; have a giant computer monitor as the main screen for no particular reason

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/Inventoried-Demo',
+  secret: 'secret'
+}
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const express = require('express');
+const router = express.Router();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+const port = 3000;
+const app = express();
+
+mongoose.connect(
+  config.database,
+  { useNewUrlParser: true}
+);
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database ${config.database}`);
+});
+mongoose.connection.on('error', (err) => {
+  console.log(`Database error: ${err}`);
+});
+
+//CORS middleware
+app.use(cors());
+//body-parse middleware
+app.use(bodyParser.json());
+
+//initialize route
+const hello = require('./routes/hello');
+app.use('/hello', hello);
+
+app.get('/', (req, res, next) => {
+  res.send('hello world');
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
